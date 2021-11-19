0957357eab8b87bfec4a8f407718ac83748801d2
taegyoo1104 <taegyoo1104@gmail.com>
2021-11-19 18:00:34 +0900

    등록 기능

---
+++ src/main/java/com/example/springpractice/service/PostsService.java
@@ -0,0 +1,20 @@
+package com.example.springpractice.service;
+
+import com.example.springpractice.domain.posts.PostsRepository;
+import com.example.springpractice.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
