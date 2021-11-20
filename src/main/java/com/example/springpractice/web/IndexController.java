2373d1a982eb44dd89c339e1b39888d0f5f0f3ee
taegyoo1104 <taegyoo1104@gmail.com>
2021-11-20 23:29:12 +0900

    등록 화면 구현

--- src/main/java/com/example/springpractice/web/IndexController.java
+++ src/main/java/com/example/springpractice/web/IndexController.java
@@ -3,11 +3,17 @@ package com.example.springpractice.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+//페이지와 관련된 컨트롤러는 모두 IndexController
 @Controller
 public class IndexController {
 
     @GetMapping("/")
     public String index() {
         return "index";                //    --> src/main/resources/templates/index.mustache로 View Resolver가 처리
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
     }
 }
