2f703f2d07eaa45dec19943d32c300470e721070
taegyoo1104 <taegyoo1104@gmail.com>
2021-11-21 00:02:45 +0900

    Index 조회 화면 구현

--- src/main/java/com/example/springpractice/service/PostsService.java
+++ src/main/java/com/example/springpractice/service/PostsService.java
@@ -2,12 +2,16 @@ package com.example.springpractice.service;
 
 import com.example.springpractice.domain.posts.Posts;
 import com.example.springpractice.domain.posts.PostsRepository;
+import com.example.springpractice.web.dto.PostsListResponseDto;
 import com.example.springpractice.web.dto.PostsResponseDto;
 import com.example.springpractice.web.dto.PostsSaveRequestDto;
 import com.example.springpractice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +39,12 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시물이 없습니다. id = " + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true) //트랜잭션 범위는 유지하되 조회 기능만 남겨 조회 속도가 개선된다. *등록,수정,삭제 기능이 없는 서비스 메소드에 사용 추천
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/example/springpractice/web/IndexController.java
+++ src/main/java/com/example/springpractice/web/IndexController.java
@@ -1,14 +1,22 @@
 package com.example.springpractice.web;
 
+import com.example.springpractice.service.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 //페이지와 관련된 컨트롤러는 모두 IndexController
 @Controller
+@RequiredArgsConstructor
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {       //서버 템플릿 엔진에서 사용할 수 있는 객체를 저장한다.
+        model.addAttribute("posts", postsService.findAllDesc());
+
         return "index";                //    --> src/main/resources/templates/index.mustache로 View Resolver가 처리
     }
 
